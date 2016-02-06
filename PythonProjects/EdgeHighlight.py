e903f4868fead0b5e7e42085cb018f16126c505d
Sarath Shekkizhar <shekkizh@usc.edu>
2016-02-05 17:36:10 -0800

    Edges in Video/Image

---
+++ PythonProjects/EdgeHighlight.py
@@ -0,0 +1,33 @@
+__author__ = 'Charlie'
+import cv2
+import numpy as np
+import sys, inspect, os
+import argparse
+
+cmd_subfolder = os.path.abspath(os.path.realpath(os.path.join(os.path.split(inspect.getfile(inspect.currentframe()))[0],"..", "Image_Lib")))
+if cmd_subfolder not in sys.path:
+    sys.path.insert(0, cmd_subfolder)
+
+import image_utils as utils
+
+ap = argparse.ArgumentParser("Highlight edges in images")
+ap.add_argument("-i", "--image", required = True, help = "Path to image file")
+ap.add_argument("-c", "--color", required = False, help = "Highlight color - Black (Default)/ White")
+args = vars(ap.parse_args())
+
+if not args.get("color", False):
+    color = (0,0,0)
+elif args["color"].lower() == "white":
+    color = (255,255,255)
+
+image = cv2.imread(args["image"])
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.GaussianBlur(gray, (5,5), 0)
+edged = cv2.Canny(gray, 50, 175)
+
+(_, cnts, _ ) = cv2.findContours(edged, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+# cnts = sorted(cnts, key = cv2.contourArea, reverse=True)
+cv2.drawContours(image,cnts, -1, color, 1)
+cv2.imshow("Output", utils.image_resize(image, height = 600))
+cv2.waitKey()
+cv2.destroyAllWindows()
--- PythonProjects/EdgesInVideo.py
+++ PythonProjects/EdgesInVideo.py
@@ -3,14 +3,15 @@ import argparse, inspect, sys, os
 import cv2
 import numpy as np
 
-cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile(inspect.currentframe()))[0],"..","Image_Lib")))
+cmd_subfolder = os.path.realpath(
+    os.path.abspath(os.path.join(os.path.split(inspect.getfile(inspect.currentframe()))[0], "..", "Image_Lib")))
 if cmd_subfolder not in sys.path:
     sys.path.insert(0, cmd_subfolder)
 
 import image_utils as utils
 
 ap = argparse.ArgumentParser("Real time edge finding on video")
-ap.add_argument("-v", "--video", help = "Path to video file. Defaults to camera if not provided")
+ap.add_argument("-v", "--video", help="Path to video file. Defaults to camera if not provided")
 args = vars(ap.parse_args())
 
 if not args.get("video", False):
@@ -18,25 +19,22 @@ if not args.get("video", False):
 else:
     camera = cv2.VideoCapture(args["video"])
 
-
 while True:
     grabbed, frame = camera.read()
     if not grabbed:
         break
 
     imageGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    result = cv2.GaussianBlur(imageGray, (5,5), 0)
-    result = cv2.Canny(result, 100,200)
-    # (_, cnts, _) = cv2.findContours(result,  cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    # cnts =sorted(cnts, key=cv2.contourArea, reverse=True)[:5]
-    #
-    # cv2.drawContours(frame, cnts, -1, (0,255,0),2)
-    #
-    # cv2.imshow("Frame", utils.image_resize(frame, height = 300))
-    cv2.imshow("Edge", utils.image_resize(result, height = 300))
+    result = cv2.GaussianBlur(imageGray, (5, 5), 0)
+    result = cv2.Canny(result, 50, 175)
 
-    if cv2.waitKey(0) & 0xFF == ord("q"):
-		break
+    (_, cnts, _) = cv2.findContours(result, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    # cnts = sorted(cnts, key=cv2.contourArea)
+    cv2.drawContours(frame, cnts, -1, (0, 0, 0), 1)
+    cv2.imshow("Edge", utils.image_resize(frame, height=500))
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
 
 camera.release()
 cv2.destroyAllWindows()
