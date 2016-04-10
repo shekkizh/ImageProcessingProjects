0568ab0db7f5a7aeec89c1ff2b4f0e9778f7bc0e
Sarath Shekkizhar <shekkizh@usc.edu>
2016-04-10 16:52:12 -0700

    Updated ref path to Image Lib

--- PythonProjects/Stereo/StereoMatching.py
+++ PythonProjects/Stereo/StereoMatching.py
@@ -6,7 +6,7 @@ import cv2
 import argparse, os, sys, inspect
 
 cmd_subfolder = os.path.realpath(
-    os.path.abspath(os.path.join(os.path.split(inspect.getfile(inspect.currentframe()))[0], "..", "Image_Lib")))
+    os.path.abspath(os.path.join(os.path.split(inspect.getfile(inspect.currentframe()))[0], "..","..", "Image_Lib")))
 if cmd_subfolder not in sys.path:
     sys.path.insert(0, cmd_subfolder)
 
