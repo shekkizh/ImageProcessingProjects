3925fb11282c9aa1f6504b3449cb1c2b048e42db
Sarath Shekkizhar <shekkizh@usc.edu>
2016-04-10 22:32:03 -0700

    Too high reprojection error - either camera has high distortion or need better resolution chessboard images

--- PythonProjects/Stereo/CameraCalibration.py
+++ PythonProjects/Stereo/CameraCalibration.py
@@ -38,9 +38,9 @@ for f in images:
         cv2.cornerSubPix(image, corners, (5,5), (-1,-1), termination)
         imgpoints.append(corners.reshape(-1,2))
 
-        # cv2.drawChessboardCorners(image, (7,9), corners, ret)
-        # cv2.imshow("Output", image)
-        # cv2.waitKey(500)
+        cv2.drawChessboardCorners(image, (7,9), corners, ret)
+        cv2.imshow("Output", image)
+        cv2.waitKey(500)
     else:
         print "Could not find chessboard corners in %s" % f
 
@@ -55,17 +55,17 @@ print distortion_coeff
 
 image_to_undistort = cv2.imread("images/camera_calibration/chessboard05.jpg", 0)
 
-# Method 1 as suggested in OpenCV docs - produces a wierd radial distorted output
-new_camera_matrix, roi = cv2.getOptimalNewCameraMatrix(camera_matrix, distortion_coeff, (w,h), 0)
-undistorted_image = cv2.undistort(image_to_undistort, camera_matrix, distortion_coeff, None, new_camera_matrix)
+# # Method 1 as suggested in OpenCV docs - produces a wierd radial distorted output
+# new_camera_matrix, roi = cv2.getOptimalNewCameraMatrix(camera_matrix, distortion_coeff, (w,h), 1)
+# undistorted_image = cv2.undistort(image_to_undistort, camera_matrix, distortion_coeff, None, new_camera_matrix)
 
 # x,y,w,h = roi
 # undistorted_image = undistorted_image[y:y+h, x:x+w]
 
-# #Method 2 - Also fails :(
-# mapx, mapy = cv2.initUndistortRectifyMap(camera_matrix, distortion_coeff,
-#                                          None, camera_matrix, (w,h), 5)
-# undistorted_image = cv2.remap(image_to_undistort, mapx, mapy, cv2.INTER_LINEAR)
+#Method 2 - Also fails :(
+mapx, mapy = cv2.initUndistortRectifyMap(camera_matrix, distortion_coeff,
+                                         None, camera_matrix, (w,h), 5)
+undistorted_image = cv2.remap(image_to_undistort, mapx, mapy, cv2.INTER_LINEAR)
 
 
 cv2.imshow('Distorted Image', image_to_undistort)
