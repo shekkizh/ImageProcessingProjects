525a6dd59980a41f1b3069a0b62e326f04578586
Sarath Shekkizhar <shekkizh@usc.edu>
2016-06-13 00:50:03 -0700

    saving results

--- PythonProjects/FootprintContour/FootprintContour.py
+++ PythonProjects/FootprintContour/FootprintContour.py
@@ -15,26 +15,30 @@ ap.add_argument("-i", "--image", required=True, help="Path to image file")
 ap.add_argument("-bg", "--background", required=True, help="Path to background file")
 args = vars(ap.parse_args())
 
-image = cv2.cvtColor(cv2.imread(args["image"]), cv2.COLOR_BGR2HSV)
-background = cv2.cvtColor(cv2.imread(args["background"]), cv2.COLOR_BGR2HSV)
+image = cv2.imread(args["image"])
+image_lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+background = cv2.imread(args["background"])
+bg_lab = cv2.cvtColor(background, cv2.COLOR_BGR2LAB)
 
 # fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
 # fgbg.apply(background)
 # output = fgbg.apply(image)
 
 
-image_gray = cv2.equalizeHist(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY))
-background_gray = cv2.equalizeHist(cv2.cvtColor(background, cv2.COLOR_BGR2GRAY))
-output = cv2.absdiff(image_gray, background_gray)
+image_gray = (cv2.cvtColor(image, cv2.COLOR_BGR2GRAY))
+background_gray = (cv2.cvtColor(background, cv2.COLOR_BGR2GRAY))
+output = image_gray - background_gray
+output[output < 128] = 0
+output[output>=128] = 255
 thresh, output = cv2.threshold(output, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-image_processed = np.ma.array(image.copy(), mask = [output,output,output])
+image_processed = np.ma.array(image_lab.copy(), mask = [output,output,output])
 # print image_processed.shape
 mean = np.mean(np.mean(image_processed, axis =0), axis=0)
 print mean
 std = np.std(np.std(image_processed, axis = 0), axis=0)
 print std
-output = cv2.inRange(image, mean - 2*std, mean + 2*std)
+output = cv2.inRange(image_lab, mean - 2*std, mean + 2*std)
 
 kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
 skinMask = cv2.erode(output, kernel)
@@ -43,8 +47,9 @@ skinMask = cv2.dilate(output, kernel)
 skinMask = cv2.GaussianBlur(output, (3, 3), 0)
 # skinMask=output
 output = cv2.bitwise_and(image, image, mask=skinMask)
-cv2.imshow("input", cv2.cvtColor(image, cv2.COLOR_HSV2BGR))
+cv2.imshow("input", image)
 cv2.imshow("Mask", skinMask)
-cv2.imshow("output", cv2.cvtColor(output, cv2.COLOR_HSV2BGR))
+cv2.imshow("output", output)
 cv2.waitKey()
 cv2.destroyAllWindows()
+cv2.imwrite("skin_segment.png", output)
